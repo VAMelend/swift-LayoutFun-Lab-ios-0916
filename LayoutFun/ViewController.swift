c3f2c06ac1983129e46ffe382e7caf141f0ba135
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-10-13 10:49:37 -0400

    done

--- LayoutFun/ViewController.swift
+++ LayoutFun/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
     let redView = UIView(frame: CGRect.zero)
     let orangeView = UIView(frame: CGRect.zero)
     let yellowView = UIView(frame: CGRect.zero)
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.gray
+        
         turnOffAutoResizingOnAllViews()
         setupTheConstraints()
         setupColors()
@@ -32,6 +33,41 @@ extension ViewController {
     
     func setupTheConstraints() {
         // TODO: Create all the constraints (in code) for the various views
+        
+        //YUP! It's upside down. 
+        
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        redView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+        
+        orangeView.translatesAutoresizingMaskIntoConstraints = false
+        orangeView.widthAnchor.constraint(equalTo: redView.widthAnchor, multiplier: 0.75).isActive = true
+        orangeView.heightAnchor.constraint(equalTo: redView.heightAnchor, multiplier: 1.0).isActive = true
+        orangeView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        orangeView.topAnchor.constraint(equalTo: redView.bottomAnchor).isActive = true
+        
+        
+        yellowView.translatesAutoresizingMaskIntoConstraints = false
+        yellowView.widthAnchor.constraint(equalTo: orangeView.widthAnchor, multiplier: 0.75).isActive = true
+        yellowView.heightAnchor.constraint(equalTo: orangeView.heightAnchor, multiplier: 1.0).isActive = true
+        yellowView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        yellowView.topAnchor.constraint(equalTo: orangeView.bottomAnchor).isActive = true
+        
+        
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.widthAnchor.constraint(equalTo: yellowView.widthAnchor, multiplier: 0.75).isActive = true
+        greenView.heightAnchor.constraint(equalTo: yellowView.heightAnchor, multiplier: 1.0).isActive = true
+        greenView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        greenView.topAnchor.constraint(equalTo: yellowView.bottomAnchor).isActive = true
+        
+        
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.widthAnchor.constraint(equalTo: greenView.widthAnchor, multiplier: 0.75).isActive = true
+        blueView.heightAnchor.constraint(equalTo: greenView.heightAnchor, multiplier: 1.0).isActive = true
+        blueView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        blueView.topAnchor.constraint(equalTo: greenView.bottomAnchor).isActive = true
+        
+        
         
         
     }
